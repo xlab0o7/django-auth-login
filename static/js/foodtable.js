75ce5f6994110b3c0f7194007f10263607d64e8b
xlab0o7 <rana.zain909@gmail.com>
2024-01-01 18:35:33 +0500

    modified:   .gitignore 	deleted:    authentication/__pycache__/__init__.cpython-311.pyc 	deleted:    authentication/__pycache__/admin.cpython-311.pyc 	deleted:    authentication/__pycache__/apps.cpython-311.pyc 	deleted:    authentication/__pycache__/models.cpython-311.pyc 	deleted:    authentication/__pycache__/tokens.cpython-311.pyc 	deleted:    authentication/__pycache__/urls.cpython-311.pyc 	deleted:    authentication/__pycache__/views.cpython-311.pyc 	deleted:    authentication/migrations/__pycache__/__init__.cpython-311.pyc 	deleted:    caloriesTracker/__pycache__/__init__.cpython-311.pyc 	deleted:    caloriesTracker/__pycache__/apps.cpython-311.pyc 	deleted:    caloriesTracker/migrations/__pycache__/0001_initial.cpython-311.pyc 	deleted:    caloriesTracker/migrations/__pycache__/0002_delete_tracker.cpython-311.pyc 	deleted:    caloriesTracker/migrations/__pycache__/__init__.cpython-311.pyc 	deleted:    db.sqlite3 	deleted:    fooddiet/__pycache__/__init__.cpython-311.pyc 	deleted:    fooddiet/__pycache__/info.cpython-311.pyc 	deleted:    fooddiet/__pycache__/urls.cpython-311.pyc 	deleted:    fooddiet/__pycache__/wsgi.cpython-311.pyc 	deleted:    fooditems/__pycache__/__init__.cpython-311.pyc 	deleted:    fooditems/__pycache__/apps.cpython-311.pyc 	deleted:    fooditems/migrations/__pycache__/0001_initial.cpython-311.pyc 	deleted:    fooditems/migrations/__pycache__/0002_fooditems_uname.cpython-311.pyc 	deleted:    fooditems/migrations/__pycache__/0003_alter_fooditems_uname.cpython-311.pyc 	deleted:    fooditems/migrations/__pycache__/__init__.cpython-311.pyc 	deleted:    static/js/foodtable.js

--- static/js/foodtable.js
+++
@@ -1,7 +0,0 @@
-const table = document.getElementById('foodtable');
-let calories = 0;
-    for (let i =1; i< table.rows.length -1; i++){
-        calories += parseFloat(table.rows[i].cells[1].innerHTML);
-    }
-
-    
